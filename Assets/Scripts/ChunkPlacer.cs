6971dff8c1f56d8834c4d9cd848da8edc5aea226
Jolengo <danil.elimarov@mail.ru>
2023-03-23 19:25:00 +0300

    Commit_
    Небольшой фикс

--- Assets/Scripts/ChunkPlacer.cs
+++ Assets/Scripts/ChunkPlacer.cs
@@ -17,7 +17,7 @@ public class ChunkPlacer : MonoBehaviour
 
     private void Update()
     {
-        if (Player.position.z > spawnedChunks[spawnedChunks.Count - 1].End.position.z - 15)
+        if (Player.position.x > spawnedChunks[spawnedChunks.Count - 1].End.position.x - 15)
         {
             SpawnChunk();
         }
@@ -25,37 +25,12 @@ public class ChunkPlacer : MonoBehaviour
 
     private void SpawnChunk()
     {
-        Chunk newChunk = Instantiate(GetRandomChunk());
-        newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].End.position - newChunk.Begin.localPosition;
+        Chunk newChunk = Instantiate(ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length)]);
+        newChunk.transform.position = spawnedChunks[spawnedChunks.Count-1].End.position;
         spawnedChunks.Add(newChunk);
 
-        if (spawnedChunks.Count >= 3)
-        {
-            Destroy(spawnedChunks[0].gameObject);
-            spawnedChunks.RemoveAt(0);
-        }
+
     }
 
-    private Chunk GetRandomChunk()
-    {
-        List<float> chances = new List<float>();
-        for (int i = 0; i < ChunkPrefabs.Length; i++)
-        {
-            chances.Add(ChunkPrefabs[i].ChanceFromDistance.Evaluate(Player.transform.position.z));
-        }
-
-        float value = Random.Range(0, chances.Sum());
-        float sum = 0;
-
-        for (int i = 0; i < chances.Count; i++)
-        {
-            sum += chances[i];
-            if (value < sum)
-            {
-                return ChunkPrefabs[i];
-            }
-        }
-
-        return ChunkPrefabs[ChunkPrefabs.Length - 1];
-    }
+   
 }
